d97c662bbf2f6f693dd8fa5489bf204796b87e0a
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-03-01 15:17:07 -0600

    Uso de while para las opciones

--- MendezAlejandra_while15/Program.cs
+++ MendezAlejandra_while15/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("----MENÚ----");
+
+int opcion = 0;
+
+while (opcion != 3)
+{
+    Console.WriteLine("1. Saludar Usuario");
+    Console.WriteLine("2. Mensaje al usuario");
+    Console.WriteLine("3. Salir");
+    Console.WriteLine("Opcion: ");
+    opcion = int.Parse(Console.ReadLine());
