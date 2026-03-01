f4261828f2f43516bcea192232506f7a6431af76
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-03-01 15:17:59 -0600

    uso de switch para las opciones

--- MendezAlejandra_while15/Program.cs
+++ MendezAlejandra_while15/Program.cs
@@ -10,3 +10,24 @@ while (opcion != 3)
     Console.WriteLine("3. Salir");
     Console.WriteLine("Opcion: ");
     opcion = int.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Hola usuario");
+            break;
+
+        case 2:
+            Console.WriteLine("Hola de nuevo =)");
+            break;
+
+        case 3:
+            Console.WriteLine("Saliendo");
+            break;
+
+        default:
+            Console.WriteLine("Opcion incorrecta");
+            break;
+    }
+    Console.WriteLine();
+}
+
